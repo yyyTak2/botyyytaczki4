68a63a22fa23767294d356020f94b50dc83524f7
Dominik <45369491+yyyTak2@users.noreply.github.com>
2018-12-22 20:46:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,13 +17,27 @@ bot.on('ready', () => {
 bot.on('message', message => {
 if(message.content.toLowerCase() === '!nekko')
    message.channel.send('Muj krul i włatca ❤️');
-    
-});
 
 bot.on('message', message => {
 if(message.content.toLowerCase() === '!strona')
    message.channel.send('**Link do strony serwera ➫ https://olekmajewski321.wixsite.com/discord**');
 
+});
+    
+module.exports.run = async (bot, message, args) => {
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
+if(!args[0]) return message.channel.send("no");
+message.channel.bulkDelete(args[0]).then(() => {
+message.channel.send(`Wyczyszczono ${args[0]} wiadomości.`).then(msg => msg.delete(2000));
+});
+
+}
+
+module.exports.help = {
+  name: "clear"
+}
+    
+    
 });
     
 bot.login(process.env.B0T_T0KEN);
