4cfe4cd9260dcaedd2aaac8e453577320046d56c
Dominik <45369491+yyyTak2@users.noreply.github.com>
2019-01-08 22:33:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,24 +2,15 @@ var Discord = require('discord.js');
 var bot = new Discord.Client();
 var client = new Discord.Client();
 
-static void UpdatePresence()
-{
-    DiscordRichPresence discordPresence;
-    memset(&discordPresence, 0, sizeof(discordPresence));
-    discordPresence.state = "Huhuhuhu";
-    discordPresence.details = "Taka kupa";
-    discordPresence.startTimestamp = 1507665886;
-    discordPresence.endTimestamp = 1507665886;
-    discordPresence.largeImageKey = "1";
-    discordPresence.largeImageText = "Numbani";
-    discordPresence.smallImageText = "Rogue - Level 100";
-    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
-    discordPresence.partySize = 1;
-    discordPresence.partyMax = 69;
-    discordPresence.spectateSecret = "MTIzNDV8MTIzNDV8MTMyNDU0";
-    discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
-    Discord_UpdatePresence(&discordPresence);
-}
+bot.on('ready', () => {
+    bot.user.setStatus('dnd')
+    bot.user.setPresence({
+        game: {
+            name: 'Huh',
+            type: 0
+        }
+    });
+});	
 
 bot.on('message', message => {
 if(message.content.toLowerCase() === '!strona')
