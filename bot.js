48d5cefa4adc977c661399755075a06edb344c9f
Dominik <45369491+yyyTak2@users.noreply.github.com>
2019-01-08 20:19:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,6 +14,25 @@ bot.on('ready', () => {
     });
 });
 
+static void UpdatePresence()
+{
+    DiscordRichPresence discordPresence;
+    memset(&discordPresence, 0, sizeof(discordPresence));
+    discordPresence.state = "Huhuhuhu";
+    discordPresence.details = "Taka kupa";
+    discordPresence.startTimestamp = 1507665886;
+    discordPresence.endTimestamp = 1507665886;
+    discordPresence.largeImageKey = "1";
+    discordPresence.largeImageText = "Numbani";
+    discordPresence.smallImageText = "Rogue - Level 100";
+    discordPresence.partyId = "ae488379-351d-4a4f-ad32-2b9b01c91657";
+    discordPresence.partySize = 1;
+    discordPresence.partyMax = 69;
+    discordPresence.spectateSecret = "MTIzNDV8MTIzNDV8MTMyNDU0";
+    discordPresence.joinSecret = "MTI4NzM0OjFpMmhuZToxMjMxMjM= ";
+    Discord_UpdatePresence(&discordPresence);
+}
+
 bot.on('message', message => {
 if(message.content.toLowerCase() === '!strona')
    message.channel.send('**Link do strony Przyjaznej Społeczności ➫ https://olekmajewski321.wixsite.com/discord**');
