75835d519715419952c18e4f1ac10ef0a374803a
Dominik <dominik.cz89.com@gmail.com>
2019-03-21 13:15:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,27 +18,16 @@ if(message.content.toLowerCase() === '!strona')
 
 });
 
-if(command === "play") {
-    if (!message.member.voiceChannel) return message.channel.send(':no_entry_sign: Please join a voice channel.');
-    if (message.guild.me.voiceChannel) return message.channel.send(':no_entry_sign: Error, the bot is already connected to another music channel or a song is playing.');
-    if (!args[0]) return message.channel.send(':no_entry_sign: Error, please enter a **URL** following the command.');
+const Discord = require('discord.js');
+const randomPuppy = require('random-puppy');
 
-    let validate = await ytdl.validateURL(args[0]);
-   
-    if (!validate) return message.channel.send(':no_entry_sign: Error, please input a __valid__ url following the command.');
-
-    let info = await ytdl.getInfo(args[0]);
-   
-    let connection = await message.member.voiceChannel.join();
-    let dispatcher = await connection.playStream(ytdl(args[0], {
-        filter: 'audioonly'
-    }));
-
-    let playembed = new Discord.RichEmbed()
-    .setTitle("Now playing")
-    .setDescription(`${info.title}`)
-    
-    message.channel.send(playembed);
-}
+randomPuppy('!memes')
+    .then(url => {
+        const embed = new Discord.MessageEmbed()
+            .setTimestamp()
+            .setImage(url)
+            .setColor('RANDOM')
+        message.channel.send(embed);
+    });
     
 bot.login(process.env.B0T_T0KEN);
